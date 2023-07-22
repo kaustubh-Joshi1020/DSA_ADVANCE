f9af474e983d23c245ff7b6a9cc84868503ea872
kaustubh-Joshi1020 <kj09032002@gmail.com>
2023-07-22 16:39:21 +0530

    push at bottom

---
+++ src/Stacks/StackPushAtBottom.java
@@ -0,0 +1,31 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class StackPushAtBottom {
+
+    public static void pushAtBottom(Stack<Integer> s , int data){
+        //base
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s ,data);
+        s.push(top);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(10);
+        s.push(11);
+        s.push(12);
+        s.push(13);
+        pushAtBottom(s , 9);
+        while (!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+
+    }
+
+}
